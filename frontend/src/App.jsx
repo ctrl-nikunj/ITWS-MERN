b516676b63674e94df0ac087deeca54ed1e50bf1
ctrl-nikunj <goyalnikunj2004@gmail.com>
2025-08-29 01:26:47 +0530

    Initial working verion for ITWS-MERN Application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import ImageProcessor from './pages/ImageProcessor'
+import './App.css'
+
+function App() {
+  return (
+    <ImageProcessor/>
+  )
+}
+
+export default App
